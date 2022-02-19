b8ba3a73127b78160d1d400afd70c3aa6fa4110d
Jacqueline Askins <askinsjacqueline@gmail.com>
2022-02-18 20:37:21 -0500

    Setup frontend react project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,3 @@
+export default function App(): JSX.Element {
+  return <h1>We Wordle</h1>;
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === "development";
+
+  return {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    devServer: {
+      hot: true,
+      open: true,
+    },
+    devtool: isDevelopment ? "cheap-module-source-map" : "source-map",
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: require.resolve("babel-loader"),
+              options: {
+                plugins: [
+                  isDevelopment && require.resolve("react-refresh/babel"),
+                ].filter(Boolean),
+              },
+            },
+          ],
+        },
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+  };
+};
