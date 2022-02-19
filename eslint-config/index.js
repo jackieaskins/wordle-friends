9a37f246d0c25fd22d43212e4116925efdf2f6ed
Jacqueline Askins <askinsjacqueline@gmail.com>
2022-02-18 20:13:23 -0500

    Add eslint, prettier, lint-staged, and husky

---
+++ eslint-config/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  reportUnusedDisableDirectives: true,
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": ["error"],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { ignoreRestSiblings: true },
+    ],
+    "arrow-body-style": ["error", "as-needed"],
+    "no-debugger": ["error"],
+  },
+};
